3fc739263539dfc44bd20eb76269721d3441b30e
Sathvik Putta <156390982+PuttaSathvik16@users.noreply.github.com>
2024-02-14 14:10:14 -0500

    Create DATA MODEL Question 4.js

---
+++ DATA MODEL Question 4.js
@@ -0,0 +1,20 @@
+//data modelling in MongoDB
+//DATA MODEL
+{
+  "_id": ObjectId("65b1553c5326bc7c7f57cf1c"),
+  "Std_ID": "0942304",
+  "Personal_details": {
+    "First_Name": "sathvik",
+    "Last_Name": "putta",
+    "Date_Of_Birth": "2001-05-16"
+  },
+  "Contact": {
+    "e-mail": "puttasathvik16@gmail.com",
+    "phone": "1234567890"
+  },
+  "Address": {
+    "city": "New Haven",
+    "Area": “171 cedar hill ave",
+    "State": "connecticut"
+  }
+}
